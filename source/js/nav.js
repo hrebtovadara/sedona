4d1c914cb883806c15a23cf0c0e12dc7489219ac
dasha <hrebtovadara@gmail.com>
2020-03-30 20:59:52 +0300

    add new js code for gallery

--- source/js/nav.js
+++ source/js/nav.js
@@ -16,25 +16,33 @@ navToggle.addEventListener("click", function () {
 });
 
 buttonClose.addEventListener("click", function () {
-    navMain.classList.add("header--closed");
-    navMain.classList.remove("header--opened");
+  navMain.classList.add("header--closed");
+  navMain.classList.remove("header--opened");
 });
-
-let galleryList = document.querySelectorAll(".gallery__item");
-
-for (let element of galleryList) {
-  element.onclick = function() {
-    let galleryImage = element.querySelector(".gallery__preview");
-    let galleryImageFull = document.querySelector(".gallery__image-full");
-    galleryImageFull.src = galleryImage.src;
-    
-  }
-}
 
 let buttonLikeList = document.querySelectorAll(".gallery__like");
 
+
 for (let element of buttonLikeList) {
-  element.onclick = function() {
-        
+  element.onclick = function () {
+    let counterLike = element.querySelector(".gallery__like-counter");
+    if (element.classList.contains("gallery__like-active")) {
+      element.classList.remove("gallery__like-active");
+      counterLike.textContent--;
+    } else {
+      element.classList.add("gallery__like-active")
+      counterLike.textContent++;
+    }
   }
 }
+
+
+let galleryList = document.querySelectorAll(".gallery__item"); 
+  for (let element of galleryList) {
+    let imageElement = element.querySelector(".gallery__image");
+    imageElement.onclick = function () {
+       element.classList.toggle("gallery__item-full");
+       let galleryFull = element.querySelector(".gallery__image");
+       galleryFull.classList.toggle("gallery__image-full");
+    }
+  }
